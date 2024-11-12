a259171b25121cd7d8bd72fa84d68e65e6e42778
sinseongseop <sss02030@naver.com>
2024-11-12 10:39:57 +0900

    Feat: 영화 추천 알림 생성

--- app/src/main/java/com/example/movieismylife/MainActivity.kt
+++ app/src/main/java/com/example/movieismylife/MainActivity.kt
@@ -1,21 +1,47 @@
 package com.example.movieismylife
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.movieismylife.ui.theme.MovieIsMyLifeTheme
 import com.example.movieismylife.viewmodel.MovieListViewModel
 import com.example.movieismylife.viewmodel.MovieReviewViewModel
 
+
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            createNotificationChannel(this)
+            showMovieRecommendationNotification(this)
+        } else {
+            Log.d("Notification", "Notification permission denied")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 앱 처음 사용시 알림 허용 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestNotificationPermission()
+        } else {
+            createNotificationChannel(this)
+            showMovieRecommendationNotification(this)
+        }
+
         enableEdgeToEdge()
         setContent {
             MovieIsMyLifeTheme {
@@ -33,10 +59,22 @@ class MainActivity : ComponentActivity() {
                 // 나중에 search button에 navigation과 함께 적용해야 함
 //                movieListViewModel.fetchSearchMovies("인생", page = 1)
 //                SearchResultScreen(movieListViewModel = movieListViewModel)
+
             }
         }
     }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        } else {
+            createNotificationChannel(this)
+            showMovieRecommendationNotification(this)
+        }
+    }
 }
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
